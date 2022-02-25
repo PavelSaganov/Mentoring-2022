bd81979a35a4977af2940b70fb4ad6a4d9503335
PavelSaganov <saganovpavel@mail.ru>
2022-02-25 08:25:54 +0300

    Initial. Created Console and WinForm app

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your name is: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {username}");
+            Console.ReadKey();
+        }
+    }
+}
