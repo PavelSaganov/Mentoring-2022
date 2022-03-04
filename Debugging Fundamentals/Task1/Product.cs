4456d1a0a279412b7ebce24953fcdba60c413aa8
PavelSaganov <saganovpavel@mail.ru>
2022-03-04 12:24:44 +0300

    Added throwing exception for BubleSort and overrided Equals, GetHashCode methods for Product

--- Debugging Fundamentals/Task1/Product.cs
+++ Debugging Fundamentals/Task1/Product.cs
@@ -11,5 +11,19 @@
         public string Name { get; set; }
 
         public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            Product productObj = obj as Product;
+            if (productObj == null)
+                return false;
+            else
+                return Name.Equals(productObj.Name) && Price.Equals(productObj.Price);
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() * Price.GetHashCode() / 12;
+        }
     }
 }
--- Debugging Fundamentals/Task1/Utilities.cs
+++ Debugging Fundamentals/Task1/Utilities.cs
@@ -10,6 +10,9 @@ namespace Task1
         /// <param name="numbers">Numbers to sort.</param>
         public static void Sort(int[] numbers)
         {
+            if (numbers is null)
+                throw new NullReferenceException("Array is null");
+
             int temp;
             for (int i = 0; i < numbers.Length; i++)
             {
@@ -35,12 +38,12 @@ namespace Task1
         /// otherwise -1.</returns>
         public static int IndexOf(Product[] products, Predicate<Product> predicate)
         {
-            for (int i = 0; i < products.Length - 1; i++)
+            for (int i = 0; i < products.Length; i++)
             {
-                var product = products[i - 1];
+                var product = products[i];
                 if (predicate(product))
                 {
-                    return --i;
+                    return i;
                 }
             }
 
