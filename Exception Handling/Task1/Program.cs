fb5a2745e0dc8a111b72c3171ed45f8932c06626
PavelSaganov <saganovpavel@mail.ru>
2022-03-27 10:55:22 +0300

    Initial. Completed Task1

---
+++ Exception Handling/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter any string:");
+            string inputed = Console.ReadLine();
+
+            Console.WriteLine($"First character of {inputed} is {GetFirstCharacterOf(inputed)}");
+        }
+
+        private static char GetFirstCharacterOf(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+                throw new Exception("String is null!");
+
+            return str.FirstOrDefault();
+        }
+    }
+}
