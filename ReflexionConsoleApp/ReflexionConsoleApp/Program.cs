c19d80a12b037e8c26b5ecf837f12b0a5a43133f
PavelSaganov <saganovpavel@mail.ru>
2022-04-11 19:40:28 +0300

    Added ConfigurationPlagin.dll file to solution

--- ReflexionConsoleApp/ReflexionConsoleApp/Program.cs
+++ ReflexionConsoleApp/ReflexionConsoleApp/Program.cs
@@ -1,26 +1,10 @@
-﻿using System;
-using ReflexionConsoleApp.Configuration;
-
-namespace ReflexionConsoleApp
+﻿namespace ReflexionConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //var config = new ConfigurationManagerProvider();
-            //config.AddSetting("TaskNumber", "1");
-
-            var config = new FileManagerProvider();
-            config.AddSetting("CountOfThreads", "1");
-
-            foreach (var item in config.ReadAllSettings())
-            {
-                Console.WriteLine(item.Value);
-            }
-
-            //config.ReadAllSettings();
-
-
+            
         }
     }
 }
