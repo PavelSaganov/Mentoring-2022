4cc720bff4519e1b20c5299205ece8136784c541
PavelSaganov <saganovpavel@mail.ru>
2022-04-15 11:20:26 +0300

    Added not null condition to ConfigProviderBase

--- ReflexionConsoleApp/ConfigPlugin/ConfigurationProviderBase.cs
+++ ReflexionConsoleApp/ConfigPlugin/ConfigurationProviderBase.cs
@@ -89,7 +89,11 @@ namespace ConfigPlugin
                 return;
 
             var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            configFile.AppSettings.Settings[key].Value = value;
+            var settings = configFile.AppSettings.Settings;
+
+            if (settings[key] == null)
+                settings.Add(key, value);
+            else settings[key].Value = value;
 
             configFile.Save(ConfigurationSaveMode.Modified);
             ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
