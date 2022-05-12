6ecd76208ffbfdfb678f6c797b56b70453553764
PavelSaganov <saganovpavel@mail.ru>
2022-05-12 22:04:10 +0300

    Create EmployeeInsertTrigger.sql

---
+++ EmployeeDb/EmployeeDb/Triggers/EmployeeInsertTrigger.sql
@@ -0,0 +1,11 @@
+﻿CREATE TRIGGER [EmployeeInsertTrigger]
+ON Employee
+FOR INSERT
+AS
+BEGIN
+	SET NOCOUNT ON
+
+	INSERT INTO Company ([Name], [AddressId]) VALUES (0, N'Lakeman Associates', 1)
+		SELECT I.CompanyName, I.AddressId
+		FROM INSERTED I
+END
