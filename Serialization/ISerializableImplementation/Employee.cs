280736a2c72c9028f42093325b7a94b237559a5d
PavelSaganov <saganovpavel@mail.ru>
2022-05-20 00:00:03 +0300

    Task2 completed

---
+++ Serialization/ISerializableImplementation/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ISerializableImplementation
+{
+    [Serializable]
+    public class Employee : ISerializable
+    {
+        public Employee()
+        {
+
+        }
+
+        public Employee(SerializationInfo info, StreamingContext context)
+        {
+            Name = info.GetString("Name123");
+            Age = info.GetInt32("Age555");
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name123", Name);
+            info.AddValue("Age555", Age);
+        }
+    }
+}
---
+++ Serialization/ISerializableImplementation/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SerializationLibrary.Serializers.FileSerializers;
+
+namespace ISerializableImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var curDir = Directory.GetCurrentDirectory();
+            var pathToFile = $"{curDir}//BinDepartment.bin";
+
+            var employees = new List<Employee>()
+            {
+                new Employee()
+                {
+                    Age = 15,
+                    Name = "Alena"
+                },
+                new Employee()
+                {
+                    Age = 29,
+                    Name = "Sergey"
+                },
+                new Employee()
+                {
+                    Age = 38,
+                    Name = "Bacho"
+                },
+            };
+
+            ShowEmployeesAndTextForEach(employees, "was wrote to bin");
+
+            var binFileSerializer = new BinaryFileSerializer();
+            binFileSerializer.Serialize(pathToFile, employees);
+            var result = binFileSerializer.Deserialize<List<Employee>>($"{pathToFile}");
+
+            if (result is List<Employee>)
+            {
+                ShowEmployeesAndTextForEach(result, "was read from bin");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void ShowEmployeesAndTextForEach(List<Employee> employees, string text)
+        {
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.Name} {text}");
+            }
+        }
+    }
+}
