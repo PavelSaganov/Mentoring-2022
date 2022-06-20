0b5106e3a271a663610d4b955d53c0db6023b67c
PavelSaganov <saganovpavel@mail.ru>
2022-06-20 03:08:41 +0300

    Added repos for EF and added hiernate to nuget

---
+++ DatabaseInteractor/DatabaseInteractor/Repository/EFRepository/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseInteractor.Repository.EFRepository
+{
+    public class ProductContext
+    {
+        public ProductContext(DbContextOptions<TicketManagementContext> options)
+                : base(options)
+        {
+        }
+
+        public DbSet<Area> Areas { get; set; }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
---
+++ DatabaseInteractor/DatabaseInteractor/Repository/EFRepository/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using DatabaseInteractor.Models;
+
+namespace DatabaseInteractor.Repository.EFRepository
+{
+    public class ProductRepository : IRepositoryAsync<Product>
+    {
+    }
+}
