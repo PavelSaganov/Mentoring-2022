9115bf5992e558f4fd37cbca29d670a1694cdc28
PavelSaganov <saganovpavel@mail.ru>
2022-07-06 00:04:19 +0300

    Implemented 1-2 tasks

---
+++ HTTPFundamentals/Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Methods available: Information\nSuccess\nRedirection\nClient error\nServer error");
+                Console.WriteLine("Input method name...");
+                string name = Console.ReadLine();
+                try
+                {
+                    HttpClient client = new HttpClient();
+                    HttpResponseMessage response = client.GetAsync($"http://localhost:8888/{name}").Result;
+                    string responseBody = response.Content.ReadAsStringAsync().Result;
+
+                    Console.WriteLine($"Response with status code {(int)response.StatusCode}. Body: {responseBody}");
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine("\nException Caught!");
+                    Console.WriteLine("Message :{0} ", e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ HTTPFundamentals/Listener/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Listener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8888/");
+            listener.Start();
+
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                HttpListenerRequest request = context.Request;
+
+                var calledMethodName = request.RawUrl.Substring(1);
+                typeof(Program).GetMethod(calledMethodName).Invoke(null, new ArrayList() { context, calledMethodName }.ToArray());
+            }
+
+            listener.Stop();
+        }
+
+        public static HttpListenerResponse Information(HttpListenerContext context, string calledMethodName)
+        {
+            // Construct a response.
+            var response = context.Response;
+            string responseString = calledMethodName;
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            response.StatusCode = (int)HttpStatusCode.Continue;
+
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+
+            // You must close the output stream.
+            output.Close();
+
+            return context.Response;
+        }
+
+        public static HttpListenerResponse Success(HttpListenerContext context, string calledMethodName)
+        {
+            // Construct a response.
+            var response = context.Response;
+            string responseString = calledMethodName;
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            response.StatusCode = (int)HttpStatusCode.OK;
+
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+
+            // You must close the output stream.
+            output.Close();
+
+            return context.Response;
+        }
+
+        public static HttpListenerResponse Redirection(HttpListenerContext context, string calledMethodName)
+        {
+            // Construct a response.
+            var response = context.Response;
+            string responseString = calledMethodName;
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            response.StatusCode = (int)HttpStatusCode.Redirect;
+
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+
+            // You must close the output stream.
+            output.Close();
+
+            return context.Response;
+        }
+
+        public static HttpListenerResponse ClientError(HttpListenerContext context, string calledMethodName)
+        {
+            // Construct a response.
+            var response = context.Response;
+            string responseString = calledMethodName;
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+
+            // You must close the output stream.
+            output.Close();
+
+            return context.Response;
+        }
+
+        public static HttpListenerResponse ServerError(HttpListenerContext context, string calledMethodName)
+        {
+            // Construct a response.
+            var response = context.Response;
+            string responseString = calledMethodName;
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+
+            // You must close the output stream.
+            output.Close();
+
+            return context.Response;
+        }
+    }
+}
