e2dc20e76834bdd26c48f6de8c256a363e3c28b1
PavelSaganov <saganovpavel@mail.ru>
2022-07-06 01:06:19 +0300

    Added CategoriesController. Added swagger & using database

---
+++ WebAPI/WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebAPI.EFRepository;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class CategoriesController : Controller
+    {
+        private readonly ApiContext apiContext;
+
+        public CategoriesController(ApiContext apiContext)
+        {
+            this.apiContext = apiContext;
+        }
+
+        [HttpGet]
+        public ActionResult Read(int id)
+        {
+            return View(apiContext.Categories.FirstOrDefault(c => c.CategoryID == id));
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categories model)
+        {
+            apiContext.Categories.Add(model);
+            apiContext.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public ActionResult Update(Categories model)
+        {
+            apiContext.Categories.Update(model);
+            apiContext.SaveChanges();
+            return Ok();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            var model = apiContext.Categories.FirstOrDefault(c => c.CategoryID == id);
+            apiContext.Categories.Remove(model);
+            apiContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ WebAPI/WebAPI/EFRepository/ApiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.EFRepository
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Products> Products { get; set; }
+
+        public DbSet<Categories> Categories { get; set; }
+    }
+}
